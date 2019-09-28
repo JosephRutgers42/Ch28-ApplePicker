fc88ca65ffc8f9f2b0bcc583a73cefec056a818c
JosephRutgers42 <55005555+JosephRutgers42@users.noreply.github.com>
2019-09-27 21:29:09 -0400

    Final Version of Apple Picker

--- Assets/Apple.cs
+++ Assets/Apple.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Apple : MonoBehaviour{
+public class Apple : MonoBehaviour
+{
+    [Header("Set in Inspector")]
     public static float bottomY = -20f;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +17,9 @@ public class Apple : MonoBehaviour{
     {
         if (transform.position.y < bottomY) {
             Destroy(this.gameObject);
+
+            ApplePicker apScript = Camera.main.GetComponent<ApplePicker>();
+            apScript.AppleDestroyed();
         }
-
-
     }
 }
--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -7,14 +7,12 @@ public class AppleTree : MonoBehaviour
     [Header("Set in Inspector")]
 
     public GameObject applePrefab;
-
     public float speed = 1f;
-
     public float leftAndRightEdge = 10f;
-
     public float chanceToChangeDirections = 0.1f;
-
     public float secondsBetweenAppleDrops = 1f;
+
+    // Start is called before the first frame update
     void Start()
     {
         Invoke("DropApple", 2f);
@@ -24,7 +22,6 @@ public class AppleTree : MonoBehaviour
         GameObject apple = Instantiate<GameObject>(applePrefab);
         apple.transform.position = transform.position;
         Invoke("DropApple", secondsBetweenAppleDrops);
-
     }
 
     // Update is called once per frame
@@ -33,21 +30,15 @@ public class AppleTree : MonoBehaviour
         Vector3 pos = transform.position;
         pos.x += speed * Time.deltaTime;
         transform.position = pos;
-
-        if (pos.x < -leftAndRightEdge)
-        {
+        if (pos.x < -leftAndRightEdge) {
             speed = Mathf.Abs(speed);
-        }
-        else if (pos.x > leftAndRightEdge)
-        {
+        } else if (pos.x > leftAndRightEdge) {
             speed = -Mathf.Abs(speed);
-        }
-        
-        
-
-        void FixedUpdate(){
-           if (Random.value < chanceToChangeDirections)
+        } 
+        void FixedUpdate() {
+            if (Random.value < chanceToChangeDirections) {
                 speed *= -1;
+            }
         }
     }
 }
